90823d1e6c3e03052fb69faaaf90ae38d8eb4216
Boutaina <boutainaboudiouane@gmail.com>
2024-03-04 22:56:14 +0100

    Uploading joedixon package and creating factory and seeder of section

--- database/factories/DoctorFactory.php
+++ database/factories/DoctorFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 use App\Models\Doctor;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Section;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Doctor>
@@ -28,6 +29,7 @@ class DoctorFactory extends Factory
             'password' => '$2y$12$FKIOYCS2l8r3iktZ4fcKYehtQjZWXTwkM1Yh7uSocjnoYfeRlYo7W', // password
             'phone' => $this->faker->phoneNumber,
             'price' => $this->faker->randomElement([100,200,300,400,500]),
+            'section_id' => Section::all()->random()->id,
         ];
     }
 }
