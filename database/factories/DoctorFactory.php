a31f671386cc5fe3d494f28e51d3044b70b9e3d2
Boutaina <boutainaboudiouane@gmail.com>
2024-03-04 17:01:55 +0100

    First commit

---
+++ database/factories/DoctorFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Doctor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Doctor>
+ */
+class DoctorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Doctor::class;
+
+    public function definition(): array
+    {
+        return [
+            //
+            
+            'name' => $this->faker->name,
+            'appointments' => $this->faker->randomElement(['Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday']),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$12$FKIOYCS2l8r3iktZ4fcKYehtQjZWXTwkM1Yh7uSocjnoYfeRlYo7W', // password
+            'phone' => $this->faker->phoneNumber,
+            'price' => $this->faker->randomElement([100,200,300,400,500]),
+        ];
+    }
+}
---
+++ routes/backend.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\SectionController;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use App\Http\Controllers\DoctorController;
+
+/*
+|--------------------------------------------------------------------------
+| backend Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/Dashboard_Admin', [DashboardController::class, 'index']);
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+
+
+   //################################ dashboard user ##########################################
+    Route::get('/dashboard/user', function () {
+        return view('Dashboard.User.dashboard');
+    })->middleware(['auth'])->name('dashboard.user');
+    //################################ end dashboard user #####################################
+
+
+
+    //################################ dashboard admin ########################################
+    Route::get('/dashboard/admin', function () {
+        return view('Dashboard.Admin.dashboard');
+    })->middleware(['auth:admin'])->name('dashboard.admin');
+
+    //################################ end dashboard admin #####################################
+    Route::middleware(['auth:admin'])->group(function () {
+
+        //############################# sections route ##########################################
+
+        Route::resource('Sections', App\Http\Controllers\Dashboard\SectionController::class);
+
+        //############################# end sections route ######################################
+           //############################# Doctors route ##########################################
+
+           Route::resource('Doctors', DoctorController::class);
+
+           //############################# end Doctors route ######################################
+
+
+    });
+    require __DIR__ . '/auth.php';
+
+    });
+
