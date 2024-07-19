5223c3942698f849c8fc298d11afaa23a09f6c7f
Boutaina <boutainaboudiouane@gmail.com>
2024-07-19 13:27:29 +0100

    send appointment on phone number

--- app/Http/Controllers/Dashboard/appointments/AppointmentController.php
+++ app/Http/Controllers/Dashboard/appointments/AppointmentController.php
@@ -7,28 +7,47 @@ use App\Models\Appointment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\AppointmentConfirmation;
+use Twilio\Rest\Client;
 
 class AppointmentController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
-        $appointments = Appointment::where('type','غير مؤكد')->get();
-        return view('Dashboard.appointments.index',compact('appointments'));
+        $appointments = Appointment::where('type', 'غير مؤكد')->get();
+        return view('Dashboard.appointments.index', compact('appointments'));
     }
 
-    public function index2(){
+    public function index2()
+    {
 
-        $appointments = Appointment::where('type','مؤكد')->get();
-        return view('Dashboard.appointments.index2',compact('appointments'));
+        $appointments = Appointment::where('type', 'مؤكد')->get();
+        return view('Dashboard.appointments.index2', compact('appointments'));
     }
 
-    public function approval(Request $request,$id){
+    public function approval(Request $request, $id)
+    {
         $appointment = Appointment::findorFail($id);
         $appointment->update([
-            'type'=>'مؤكد',
-            'appointment'=>$request->appointment
+            'type' => 'مؤكد',
+            'appointment' => $request->appointment
         ]);
-        Mail::to($appointment->email)->send(new AppointmentConfirmation($appointment->name,$appointment->appointment));
+        //send mail
+        Mail::to($appointment->email)->send(new AppointmentConfirmation($appointment->name, $appointment->appointment));
+
+        // send message 
+        $receiverNumber = $appointment->phone;
+        $message = "عزيزي المريض" . " " . $appointment->name . " " . "تم حجز موعدك بتاريخ " . $appointment->appointment;
+
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_TOKEN");
+        $twilio_number = getenv("TWILIO_FROM");
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create($receiverNumber, [
+            'from' => $twilio_number,
+            'body' => $message
+        ]);
+
         session()->flash('add');
         return back();
     }
