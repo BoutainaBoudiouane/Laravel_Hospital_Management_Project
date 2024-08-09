e76eb4cd92a1a53566abf95be54767cc441f2bf1
Boutaina <boutainaboudiouane@gmail.com>
2024-08-09 17:14:13 +0100

    adding cards to the admin dashboard

---
+++ resources/lang/ar/Traduction.php
@@ -0,0 +1,9 @@
+<?php
+
+return array (
+    'Number_of_Doctors'=>'عدد الاطباء ',
+    'Number_of_Patients'=>'عدد المرضي',
+    'Number_of_Radiology_Staff'=>'عدد موظفي الاشعة',
+    'Number_of_Examination_Staff'=>' عدد موظفي الفحوصات',
+
+);
---
+++ resources/lang/en/Traduction.php
@@ -0,0 +1,8 @@
+<?php
+
+return array (
+    'Number_of_Doctors'=>'Number of Doctors ',
+    'Number_of_Patients'=>'Number of Patients',
+    'Number_of_Radiology_Staff'=>'Number of Radiology Staff',
+    'Number_of_Examination_Staff'=>'Number of Examination Staff',
+);
