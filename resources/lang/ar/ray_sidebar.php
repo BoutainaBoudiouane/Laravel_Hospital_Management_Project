0ac244ff61355579004be4ed6e7ffaa2adacad14
Boutaina <boutainaboudiouane@gmail.com>
2024-08-29 11:47:38 +0100

    modifying in the Ray Employee dashboard

---
+++ resources/lang/ar/ray_sidebar.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+   
+    'ray_reports' => 'كشوفات الاشعة',
+    'reports_list' => 'قائمة الكشوفات',
+    'completed_reports' => 'قائمة الكشوفات المكتملة',
+    'ray_employee_dashboard' => 'لوحة تحكم موظفي الاشعة',
+    'total_invoices' => 'اجمالي عدد الفواتير',
+    'invoices_in_progress' => 'اجمالي عدد الفواتير تحت الاجراء',
+    'completed_invoices' => 'اجمالي عدد الفواتير المكتملة',
+    'latest_invoices' => 'اخر 5 فواتير على النظام',
+    'invoice_date' => 'تاريخ الفاتورة',
+    'patient_name' => 'اسم المريض',
+    'doctor_name' => 'اسم الطبيب',
+    'description' => 'المطلوب',
+    'invoice_status' => 'حالة الفاتورة',
+    'under_processing' => 'تحت الاجراء',
+    'completed' => 'مكتملة',
+    'no_data_available' => 'لاتوجد بيانات',
+    'welcome_back' => 'مرحبًا بعودتك',
+];
---
+++ resources/lang/en/ray_sidebar.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+   
+    'ray_reports' => 'Radiology Reports',
+    'reports_list' => 'Reports List',
+    'completed_reports' => 'Completed Reports',
+    'ray_employee_dashboard' => 'Ray Employee Dashboard',
+    'total_invoices' => 'Total Invoices',
+    'invoices_in_progress' => 'Invoices in Progress',
+    'completed_invoices' => 'Completed Invoices',
+    'latest_invoices' => 'Latest 5 Invoices',
+    'invoice_date' => 'Invoice Date',
+    'patient_name' => 'Patient Name',
+    'doctor_name' => 'Doctor Name',
+    'description' => 'Description',
+    'invoice_status' => 'Invoice Status',
+    'under_processing' => 'Under Processing',
+    'completed' => 'Completed',
+    'no_data_available' => 'No data available',
+    'welcome_back' => 'Welcome Back',
+];
